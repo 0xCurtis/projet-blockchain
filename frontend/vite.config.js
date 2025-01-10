e0fc404e3fce095d85fee7750531cc7c33ca3559
jeremiegermond <jeremie.germond@epitech.eu>
2025-01-10 14:14:49 +0100

    basic integration get infos

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,20 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react-swc'
+import { resolve, join } from "path";
+import tailwindcss from "tailwindcss";
+import autoprefixer from "autoprefixer";
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '~' : resolve(join(__dirname, 'src'))
+    },
+  },
+  css: {
+    postcss: {
+      plugins: [tailwindcss(), autoprefixer()],
+    },
+  }
 })
